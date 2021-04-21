c8476c81969bcf855ef9fdf563d08de59ea67759
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-04-21 21:14:27 +0700

    Revert "updated no 1"
    This reverts commit ffe4aa231a85de5492282e8eea9c932c4e98c890.

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <dirent.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <wait.h>
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <stdlib.h> 
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/prctl.h>
+#include <stddef.h>
+
+
 
 int main() {
 
@@ -50,7 +45,7 @@ char *filename[3] = {"Pyoto","Musyik","Fylm"};
 			execv("/usr/bin/wget", arg);
 			
 		}
-		  
+		sleep(2);
 			
 			
 	int status_2;
@@ -61,7 +56,7 @@ char *filename[3] = {"Pyoto","Musyik","Fylm"};
 			char *arg[] = {"Wget", "--no-check-certificate","https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download","-O" ,"Musik_for_Stevany.zip","done", NULL};
 			execv("/usr/bin/wget", arg);
 		}
-		  
+		sleep(2);
 			
 	int status_3;
 	while(wait(&status_3) > 0);
@@ -71,7 +66,7 @@ char *filename[3] = {"Pyoto","Musyik","Fylm"};
 			char *arg[] = {"Wget", "--no-check-certificate","https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download","-O" ,"Foto_for_Stevany.zip","done", NULL};
 			execv("/usr/bin/wget", arg);
 		}
-		  
+		sleep(2);
 		
 	int status_4;
 	while(wait(&status_4) > 0);
@@ -81,7 +76,6 @@ char *filename[3] = {"Pyoto","Musyik","Fylm"};
 			char *arg[] = {"unzip", "-q", "Musik_for_Stevany.zip","-d","Musyik", NULL};
 			execv("/usr/bin/unzip", arg);
 		}
-		  
 		
 	int status_5;
 	while(wait(&status_5) > 0);
@@ -91,7 +85,6 @@ char *filename[3] = {"Pyoto","Musyik","Fylm"};
 			char *arg[] = {"unzip", "-q", "Film_for_Stevany.zip","-d","Fylm", NULL};
 			execv("/usr/bin/unzip", arg);
 		}
-		  
 
 	int status_6;
 	while(wait(&status_6) > 0);
@@ -101,96 +94,16 @@ char *filename[3] = {"Pyoto","Musyik","Fylm"};
 			char *arg[] = {"unzip", "-q", "Foto_for_Stevany.zip","-d","Pyoto",NULL};
 			execv("/usr/bin/unzip", arg);
 		}
-		  
 		
-	/* tried to move not working */
+	/* tried to move not working
 	
 	int status_7;
 	while(wait(&status_7) > 0);
 		pid_t pid7 = fork();
-		if (pid7 == 0) {                 
-    			DIR *folder1;
-    			DIR *folder2;
-    			DIR *folder3;
-    			struct dirent *entry;
-    			int files = 0;
-
-    			char dir[255] = "/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Pyoto/FOTO/", 
-    			dir2[255],
-         		dir3[255]="/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Fylm/FILM/",
-         		dir4[255]="/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Musyik/MUSIK/";
-
-   			folder1 = opendir("/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Pyoto/FOTO/");
-   			folder2 = opendir("/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Fylm/FILM/");
-   			folder3 = opendir("/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Musyik/MUSIK/");
-                    
-    			if(folder1 == NULL) {
-        			perror("Unable to read directory");
-        			return(1);
-    			}
-                                
-    			while( (entry=readdir(folder1))) {
-                        
-        		files++;
-       	 	printf("File %3d: | Name : %s\n", files, entry->d_name);
-
-		        if (!strcmp (entry->d_name, "."))
-         		   continue;
-      		 	if (!strcmp (entry->d_name, ".."))    
-            		   continue;
-            		strcpy(dir2,dir);
-        		strcat(dir2,entry->d_name);
-        		
-        		pid_t pid8 = fork();
-        		
-        		if (pid8 < 0) exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
-
-		        if (pid8 == 0 && entry->d_type == 8) {
- 		           char *argv[] = {"mv",dir2,"/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Pyoto/", NULL};
- 		           execv("/bin/mv", argv);
-		        }
-        		
-        		while( (entry=readdir(folder2))) {
-                        
-        		files++;
-       	 	printf("File %3d: | Name : %s\n", files, entry->d_name);
-
-		        if (!strcmp (entry->d_name, "."))
-         		   continue;
-      		 	if (!strcmp (entry->d_name, ".."))    
-            		   continue;
-            		strcpy(dir2,dir3);
-        		strcat(dir2,entry->d_name);
-
-        		pid_t pid9;
-        		pid9 = fork();
-                                    
-		        if (pid9 == 0) {
-            		    char *argv[] = {"mv",dir2,"/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Fylm/", NULL};
-                           execv("/bin/mv", argv);
-        		}
-	    		}
-	    		
-	    		while( (entry=readdir(folder3))) {
-                        
-        		files++;
-       	 	printf("File %3d: | Name : %s\n", files, entry->d_name);
-
-		        if (!strcmp (entry->d_name, "."))
-         		   continue;
-      		 	if (!strcmp (entry->d_name, ".."))    
-            		   continue;
-            		strcpy(dir2,dir4);
-        		strcat(dir2,entry->d_name);
-
-        		pid_t pid10;
-        		pid10 = fork();
-                                    
-		        if (pid10 == 0) {
-            		    char *argv[] = {"mv",dir2,"/home/xyncz/Documents/GitHub/soal-shift-sisop-modul-2-I08-2021/soal1/Musyik/", NULL};
-                           execv("/bin/mv", argv);
-        		}
-	    		}
+		if(pid7 < 0) exit(0);
+		if(pid7 == 0) {
+			char *arg[] = {"cp",  "-r", "/Pyoto/FOTO/", "/Pyoto/",NULL};
+			execv("/usr/bin/cp", arg);
 		}
-	}
+	*/
 }
